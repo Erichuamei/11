1e68d8573e7415a7d804ba015ca837f9ea8e9eea
Erichuamei <wyflbwc@163.com>
2026-09-16 14:40:52 +0800

    portal 9/16-r1: P0 multi-select paste/delete aligned with board table1

---
+++ config.js
@@ -0,0 +1,7 @@
+window.T11PortalConfig = {
+  supabaseUrl: "https://rufrowzadoimncobgyso.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZnJvd3phZG9pbW5jb2JneXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4OTk5MzksImV4cCI6MjEwMzQ3NTkzOX0.fypd_Mj4DCwt2VnQHPpkKU1EvjWGwpXFU5xCBCHu9-A",
+  editPassword: "22",
+  adminPassword: "2222",
+  legacyPasswords: ["11"]
+};
